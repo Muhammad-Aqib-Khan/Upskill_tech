a94101a6a117e53e447014e3ea93f3307e5fec57
Muhammad-Aqib-Khan <raiaqibkhan545@gmail.com>
2025-05-10 21:37:19 +0500

    title of the application and a function is changed

--- app/(components)/topNav/topNav.tsx
+++ app/(components)/topNav/topNav.tsx
@@ -32,14 +32,14 @@ export default function TopNav({ showTopNav }: TopNavBarProps) {
                 +1 234 567 8900
               </a>
               <a
-                href="mailto:info@upskill.edu"
+                href="upskilltechtandlianwala@gmail.com"
                 className="flex items-center hover:text-blue-200 transition-colors"
               >
                 <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
-                info@upskill.edu
+                upskilltechtandlianwala@gmail.com
               </a>
             </div>
 
